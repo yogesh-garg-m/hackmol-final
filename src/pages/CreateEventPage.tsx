3035d203fc77a88a5ef7f28a81711340d14e9717
devang101112 <devangsamor63@gmail.com>
2025-04-10 16:16:16 +0530

    create event form

---
+++ src/pages/CreateEventPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CreateEventForm from "@/components/events/CreateEventForm";
+
+const CreateEventPage = () => {
+  return <CreateEventForm />;
+};
+
+export default CreateEventPage;
